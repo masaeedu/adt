df60664a1e1d83ace5b59fb8534abf50a72b38fb
Asad Saeeduddin <masaeedu@gmail.com>
2018-09-24 19:48:38 -0400

    Add additional tests

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
 const { test } = require("ava")
 const { create: adt, match } = require(".")
 
-test('can create Maybe type', t => {
-  const { Nothing, Just } = adt({ Nothing: [], Just: ["a"] })
-  const map = f => match({
-    Nothing,
-    Just: x => Just(f(x))
-  })
-  const show = match({ Nothing: '(Nothing)', Just: x => `(Just ${x})` })
+test("can create Maybe type", t => {
+  const Maybe = adt({ Nothing: [], Just: ["a"] })
+  const { Nothing, Just } = Maybe
 
-  t.is(show(map(x => x * 2)(Just(21))), '(Just 42)')
+  const v = Just(21)
+
+  // Manipulate the value
+  const show = match({ Nothing: "(Nothing)", Just: x => `(Just ${x})` })
+  const map = f => match({ Nothing, Just: x => Just(f(x)) })
+  t.is(show(map(x => x * 2)(v)), "(Just 42)")
+
+  // Ensure the law is satisfied for some selected inputs
+  const inputs = [Nothing, Just(42)]
+  for (const i of inputs) {
+    const id = match(Maybe)
+    t.is(show(id(i)), show(i))
+  }
 })
